227f500947986e1a91d524a1ab712db6d8ddf058
seekartech <kataraindahmutiara@gmail.com>
2025-03-10 14:40:49 +0700

    Add frontend/src/components/Tooltip/Tooltip.js 1741592448713

--- frontend/src/components/Tooltip/Tooltip.js
+++ frontend/src/components/Tooltip/Tooltip.js
@@ -1,23 +1,5 @@
 import React from 'react';
-import { Tooltip as MuiTooltip, styled } from '@mui/material';
-
-// Custom styled tooltip
-const CustomTooltip = styled(({ className, ...props }) => (
-  <MuiTooltip {...props} classes={{ popper: className }} />
-))(({ theme }) => ({
-  '& .MuiTooltip-tooltip': {
-    backgroundColor: theme.palette.grey[800],
-    color: theme.palette.common.white,
-    fontSize: 12,
-    padding: '8px 12px',
-    borderRadius: 6,
-    maxWidth: 300,
-    boxShadow: theme.shadows[2],
-  },
-  '& .MuiTooltip-arrow': {
-    color: theme.palette.grey[800],
-  },
-}));
+import { Tooltip as MuiTooltip } from '@mui/material';
 
 function Tooltip({
   children,
@@ -28,13 +10,8 @@ function Tooltip({
   leaveDelay = 0,
   ...props
 }) {
-  // Don't render tooltip if title is empty
-  if (!title) {
-    return children;
-  }
-
   return (
-    <CustomTooltip
+    <MuiTooltip
       title={title}
       placement={placement}
       arrow={arrow}
@@ -42,13 +19,8 @@ function Tooltip({
       leaveDelay={leaveDelay}
       {...props}
     >
-      {/* Wrap children in span if it's a string or number */}
-      {typeof children === 'string' || typeof children === 'number' ? (
-        <span>{children}</span>
-      ) : (
-        children
-      )}
-    </CustomTooltip>
+      {children}
+    </MuiTooltip>
   );
 }
 
